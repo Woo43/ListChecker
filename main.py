ef8f40848a8bb10b946914bd6ae599aca13decd3
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-05-08 19:54:26 -0700

    Upadates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+cool_thing = open("final.txt","w")
+cool_thing.write("")
 a_list = open("list.txt").readlines()
 dedupelist = list(dict.fromkeys(a_list))
-cool_thing = open("final.txt","w")
 cool_thing.write(str(dedupelist))
